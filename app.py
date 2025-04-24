7d6c43e2ce48e4a0458a47121fc6060de4309238
AshNicolus <146452509+AshNicolus@users.noreply.github.com>
2025-04-24 14:13:36 +0530

    UI

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from langgraph_app import build_langgraph
+
+app = build_langgraph()
+
+st.title("Research Agent and Answer Generator")
+
+
+topic = st.text_input("Enter your research topic:")
+
+#
+if topic:
+
+    with st.spinner('Searching and generating answer...'):
+
+        result = app.invoke({"input": topic})
+
+    st.subheader("Final Answer:")
+    st.write(result["output"])
